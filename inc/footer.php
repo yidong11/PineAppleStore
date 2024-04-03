0d0f01e13b7e541a0638c6a13253a0abb2ae89ea
Cralence <mahlerrrr76@gmail.com>
2024-04-03 17:22:42 +0800

    finished header and footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,98 +1,60 @@
-</div>
-   <div class="footer">
-   	  <div class="wrapper">	
-	     <div class="section group">
-				<div class="col_1_of_4 span_1_of_4">
-						<h4>Information</h4>
-						<ul>
-						<li><a href="#">About Us</a></li>
-						<li><a href="#">Customer Service</a></li>
-						<li><a href="#"><span>Advanced Search</span></a></li>
-						<li><a href="#">Orders and Returns</a></li>
-						<li><a href="#"><span>Contact Us</span></a></li>
-						</ul>
-					</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Why buy from us</h4>
-						<ul>
-						<li><a href="about.php">About Us</a></li>
-						<li><a href="faq.php">Customer Service</a></li>
-						<li><a href="#">Privacy Policy</a></li>
-						<li><a href="contact.php"><span>Site Map</span></a></li>
-						<li><a href="defaults.php"><span>Search Terms</span></a></li>
-						</ul>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>My account</h4>
-						<ul>
-							<li><a href="contact.php">Sign In</a></li>
-							<li><a href="index.php">View Cart</a></li>
-							<li><a href="#">My Wishlist</a></li>
-							<li><a href="#">Track My Order</a></li>
-							<li><a href="faq.php">Help</a></li>
-						</ul>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Contact</h4>
-						<ul>
-							<li><span>+88-01622425286</span></li>
-							<li><span>+88-01762100187</span></li>
-						</ul>
-						<div class="social-icons">
-							<h4>Follow Us</h4>
-					   		  <ul>
-							      <li class="facebook"><a href="#" target="_blank"> </a></li>
-							      <li class="twitter"><a href="#" target="_blank"> </a></li>
-							      <li class="googleplus"><a href="#" target="_blank"> </a></li>
-							      <li class="contact"><a href="#" target="_blank"> </a></li>
-							      <div class="clear"></div>
-						     </ul>
-   	 					</div>
-				</div>
-			</div>
-			
-			<div class="payment_top">
-			  <div class="payment">
-			<img src="images/payment.jpg" alt="payment" />
-			</div>
-			</div>
-			
-			<div class="copy_right">
-				<p>Sup online shopping &amp; All rights Reseverd </p>
-		   </div>
-     </div>
+<footer class="homepage-footer">
+          <div class="homepage-container15">
+            <a href="index.php" class="homepage-navlink4">
+              <img
+                alt="logo"
+                src="public/Pineapple Icons/logo_no_bg-1400w.png"
+                class="homepage-image6"
+              />
+            </a>
+            <span class="homepage-text23">
+              <span>© 2024 PineApple,</span>
+              <br />
+              <span>All Rights Reserved.</span>
+            </span>
+          </div>
+          <div class="homepage-links-container">
+            <div class="homepage-container16">
+              <div class="homepage-contact">
+                <span class="homepage-text27">Contact Us</span>
+                <span class="homepage-text28">hello@pineapple.hk</span>
+                <span>+852 1234 5678</span>
+              </div>
+              <div class="homepage-socials">
+                <span class="homepage-text30">Follow Us</span>
+                <div class="homepage-icon-group">
+                  <svg
+                    viewBox="0 0 950.8571428571428 1024"
+                    class="homepage-icon06"
+                  >
+                    <path
+                      d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"
+                    ></path></svg
+                  ><svg
+                    viewBox="0 0 877.7142857142857 1024"
+                    class="homepage-icon08"
+                  >
+                    <path
+                      d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"
+                    ></path></svg
+                  ><svg
+                    viewBox="0 0 602.2582857142856 1024"
+                    class="homepage-icon10"
+                  >
+                    <path
+                      d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"
+                    ></path>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
     </div>
-    <script type="text/javascript">
-		$(document).ready(function() {
-			/*
-			var defaults = {
-	  			containerID: 'toTop', // fading element id
-				containerHoverID: 'toTopHover', // fading element hover id
-				scrollSpeed: 1200,
-				easingType: 'linear' 
-	 		};
-			*/
-			
-			$().UItoTop({ easingType: 'easeOutQuart' });
-			
-		});
-	</script>
-    <a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
-    <link href="css/flexslider.css" rel='stylesheet' type='text/css' />
-	  <script defer src="js/jquery.flexslider.js"></script>
-	  <script type="text/javascript">
-		$(function(){
-		  SyntaxHighlighter.all();
-		});
-		$(window).load(function(){
-		  $('.flexslider').flexslider({
-			animation: "slide",
-			start: function(slider){
-			  $('body').removeClass('loading');
-			}
-		  });
-		});
-	  </script>
-
-</body>
+    <script
+      defer=""
+      src="https://unpkg.com/@teleporthq/teleport-custom-scripts"
+    ></script>
+  </body>
 </html>
---
+++ index.php
@@ -0,0 +1,480 @@
+<?php include 'inc/header.php';?>
+
+
+<div class="homepage-main">
+  <div
+    data-thq="slider"
+    data-navigation="true"
+    data-pagination="true"
+    class="homepage-slider swiper"
+  >
+    <div data-thq="slider-wrapper" class="swiper-wrapper">
+      <div
+        data-thq="slider-slide"
+        class="homepage-slider-slide swiper-slide"
+      >
+        <div class="homepage-hero">
+          <div class="homepage-container03">
+            <h1 class="homepage-text05">IPhone 15 Pro Max</h1>
+            <div class="homepage-container04">
+              <span class="homepage-text06">FROM</span>
+              <span class="homepage-text07">HKD8599</span>
+            </div>
+            <div class="homepage-btn-group">
+              <button class="button">Explore the collection</button>
+            </div>
+          </div>
+          <img
+            alt="image23271449"
+            src="public/external/15%20promax-200w-1500w.jpg"
+            class="homepage-image"
+          />
+        </div>
+      </div>
+      <div
+        data-thq="slider-slide"
+        class="homepage-slider-slide1 swiper-slide"
+      >
+        <div class="homepage-hero1">
+          <div class="homepage-container05">
+            <h1 class="homepage-text08">IPhone 15 Plus</h1>
+            <div class="homepage-container06">
+              <span class="homepage-text09">FROM</span>
+              <span class="homepage-text10">HKD6599</span>
+            </div>
+            <div class="homepage-btn-group1">
+              <button class="button">Explore the collection</button>
+            </div>
+          </div>
+          <img
+            alt="image23271449"
+            src="public/external/15plus-200w-2400h.jpg"
+            class="homepage-image1"
+          />
+        </div>
+      </div>
+      <div
+        data-thq="slider-slide"
+        class="homepage-slider-slide2 swiper-slide"
+      >
+        <div class="homepage-hero2">
+          <div class="homepage-container07">
+            <h1 class="homepage-text11">IPhone 15 Pro</h1>
+            <div class="homepage-container08">
+              <span class="homepage-text12">FROM</span>
+              <span class="homepage-text13">HKD7599</span>
+            </div>
+            <div class="homepage-btn-group2">
+              <button class="button">Explore the collection</button>
+            </div>
+          </div>
+          <img
+            alt="image23271449"
+            src="public/Pineapple Icons/15pro-2400h.jpg"
+            class="homepage-image2"
+          />
+        </div>
+      </div>
+      <div
+        data-thq="slider-slide"
+        class="homepage-slider-slide3 swiper-slide"
+      >
+        <div class="homepage-hero3">
+          <div class="homepage-container09">
+            <h1 class="homepage-text14">IPhone 15 Pro Max</h1>
+            <div class="homepage-container10">
+              <span class="homepage-text15">FROM</span>
+              <span class="homepage-text16">HKD8599</span>
+            </div>
+            <div class="homepage-btn-group3">
+              <button class="button">Explore the collection</button>
+            </div>
+          </div>
+          <img
+            alt="image23271449"
+            src="public/external/15%20promax-200w-1500w.jpg"
+            class="homepage-image3"
+          />
+        </div>
+      </div>
+      <div
+        data-thq="slider-slide"
+        class="homepage-slider-slide4 swiper-slide"
+      >
+        <div class="homepage-hero4">
+          <div class="homepage-container11">
+            <h1 class="homepage-text17">IPhone 15 Plus</h1>
+            <div class="homepage-container12">
+              <span class="homepage-text18">FROM</span>
+              <span class="homepage-text19">HKD6599</span>
+            </div>
+            <div class="homepage-btn-group4">
+              <button class="button">Explore the collection</button>
+            </div>
+          </div>
+          <img
+            alt="image23271449"
+            src="public/external/15plus-200w-2400h.jpg"
+            class="homepage-image4"
+          />
+        </div>
+      </div>
+      <div
+        data-thq="slider-slide"
+        class="homepage-slider-slide5 swiper-slide"
+      >
+        <div class="homepage-hero5">
+          <div class="homepage-container13">
+            <h1 class="homepage-text20">IPhone 15 Pro</h1>
+            <div class="homepage-container14">
+              <span class="homepage-text21">FROM</span>
+              <span class="homepage-text22">HKD7599</span>
+            </div>
+            <div class="homepage-btn-group5">
+              <button class="button">Explore the collection</button>
+            </div>
+          </div>
+          <img
+            alt="image23271449"
+            src="public/Pineapple Icons/15pro-2400h.jpg"
+            class="homepage-image5"
+          />
+        </div>
+      </div>
+    </div>
+    <div
+      data-thq="slider-pagination"
+      class="homepage-slider-pagination swiper-pagination swiper-pagination-bullets swiper-pagination-horizontal"
+    >
+      <div
+        data-thq="slider-pagination-bullet"
+        class="swiper-pagination-bullet"
+      ></div>
+      <div
+        data-thq="slider-pagination-bullet"
+        class="swiper-pagination-bullet"
+      ></div>
+      <div
+        data-thq="slider-pagination-bullet"
+        class="swiper-pagination-bullet"
+      ></div>
+      <div
+        data-thq="slider-pagination-bullet"
+        class="swiper-pagination-bullet"
+      ></div>
+      <div
+        data-thq="slider-pagination-bullet"
+        class="swiper-pagination-bullet"
+      ></div>
+      <div
+        data-thq="slider-pagination-bullet"
+        class="swiper-pagination-bullet"
+      ></div>
+    </div>
+    <div data-thq="slider-button-prev" class="swiper-button-prev"></div>
+    <div data-thq="slider-button-next" class="swiper-button-next"></div>
+  </div>
+  <div class="homepage-trending-items section-container">
+    <div class="max-width-container">
+      <div class="section-heading-section-heading">
+        <h1 class="section-heading-text Heading-2">
+          <span>TRENDING ITEMS</span>
+        </h1>
+        <span class="section-heading-text1">
+          <span>
+            Explore our monthly most trending products, new items and
+            the best PineApple offers you can buy
+          </span>
+        </span>
+      </div>
+      <div class="homepage-gallery">
+        <div class="homepage-left">
+          <div
+            class="item-card-gallery-card item-card-root-class-name4"
+          >
+            <img
+              alt="image"
+              src="public/external/15%20promax-200w-1500w.jpg"
+              class="item-card-image"
+            />
+            <div class="item-card-container">
+              <h3 class="item-card-text">
+                <span>IPhone 15 Pro Max</span>
+              </h3>
+              <div class="item-card-container1">
+                <svg viewBox="0 0 1024 1024" class="item-card-icon">
+                  <path
+                    d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                  ></path></svg
+                ><svg viewBox="0 0 1024 1024" class="item-card-icon02">
+                  <path
+                    d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                  ></path></svg
+                ><svg viewBox="0 0 1024 1024" class="item-card-icon04">
+                  <path
+                    d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                  ></path></svg
+                ><svg viewBox="0 0 1024 1024" class="item-card-icon06">
+                  <path
+                    d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                  ></path></svg
+                ><svg viewBox="0 0 1024 1024" class="item-card-icon08">
+                  <path
+                    d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                  ></path>
+                </svg>
+              </div>
+              <div class="item-card-container2">
+                <span class="item-card-currency"><span>HKD</span></span>
+                <span class="item-card-value"><span>8599</span></span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="homepage-right">
+          <div class="homepage-top">
+            <div class="homepage-left1">
+              <div
+                class="item-card-gallery-card item-card-root-class-name2"
+              >
+                <img
+                  alt="image"
+                  src="public/external/15plus-200w-2400h.jpg"
+                  class="item-card-image"
+                />
+                <div class="item-card-container">
+                  <h3 class="item-card-text">
+                    <span>IPhone 15 Plus</span>
+                  </h3>
+                  <div class="item-card-container1">
+                    <svg viewBox="0 0 1024 1024" class="item-card-icon">
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon02"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon04"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon06"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon08"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div class="item-card-container2">
+                    <span class="item-card-currency">
+                      <span>HKD</span>
+                    </span>
+                    <span class="item-card-value">
+                      <span>6599</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="homepage-right1">
+              <div
+                class="item-card-gallery-card item-card-root-class-name3"
+              >
+                <img
+                  alt="image"
+                  src="public/Pineapple Icons/15pro-2400h.jpg"
+                  class="item-card-image"
+                />
+                <div class="item-card-container">
+                  <h3 class="item-card-text">
+                    <span>IPhone 15 Pro</span>
+                  </h3>
+                  <div class="item-card-container1">
+                    <svg viewBox="0 0 1024 1024" class="item-card-icon">
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon02"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon04"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon06"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon08"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div class="item-card-container2">
+                    <span class="item-card-currency">
+                      <span>HKD</span>
+                    </span>
+                    <span class="item-card-value">
+                      <span>7599</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="homepage-bottom">
+            <div class="homepage-left2">
+              <div
+                class="item-card-gallery-card item-card-root-class-name1"
+              >
+                <img
+                  alt="image"
+                  src="public/15-2400h.jpg"
+                  class="item-card-image"
+                />
+                <div class="item-card-container">
+                  <h3 class="item-card-text"><span>IPhone 15</span></h3>
+                  <div class="item-card-container1">
+                    <svg viewBox="0 0 1024 1024" class="item-card-icon">
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon02"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon04"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon06"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon08"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div class="item-card-container2">
+                    <span class="item-card-currency">
+                      <span>HKD</span>
+                    </span>
+                    <span class="item-card-value">
+                      <span>5599</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="homepage-right2">
+              <div
+                class="item-card-gallery-card item-card-root-class-name6"
+              >
+                <img
+                  alt="image"
+                  src="public/Pineapple Icons/iphone-2400h.png"
+                  class="item-card-image"
+                />
+                <div class="item-card-container">
+                  <h3 class="item-card-text">
+                    <span>IPhone 15 Pro Max</span>
+                  </h3>
+                  <div class="item-card-container1">
+                    <svg viewBox="0 0 1024 1024" class="item-card-icon">
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon02"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon04"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon06"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path></svg
+                    ><svg
+                      viewBox="0 0 1024 1024"
+                      class="item-card-icon08"
+                    >
+                      <path
+                        d="M512 736l-264 160 70-300-232-202 306-26 120-282 120 282 306 26-232 202 70 300z"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div class="item-card-container2">
+                    <span class="item-card-currency">
+                      <span>HKD</span>
+                    </span>
+                    <span class="item-card-value">
+                      <span>8599</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<?php include 'inc/footer.php';?>
